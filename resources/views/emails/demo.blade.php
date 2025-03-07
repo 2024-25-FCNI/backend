6eaf239d34aca270041915d0808146fede0dcb2e
nedairisz <iriszneda@gmail.com>
2025-03-07 10:25:02 +0100

    email jav

--- resources/views/emails/demo.blade.php
+++ resources/views/emails/demo.blade.php
@@ -6,14 +6,17 @@
     <title>Fizetési visszaigazolás</title>
 </head>
 <body>
-    <h1>{{ $mailData['title'] }}</h1>
-    <p>{{ $mailData['body'] }}</p>
+    <h1>Fizetési visszaigazolás</h1>
 
+    <p>Köszönjük a vásárlását, {{ $mailData['name'] }}!</p> <!-- 🔥 A felhasználó neve bekerül -->
+    
     <h3>Rendelési adatok:</h3>
     <ul>
         @foreach ($mailData['kosar'] as $termek)
-            <li>{{ $termek['cim'] }} - {{ $termek['ar'] }} Ft</li>
+            <li>{{ $termek['cim'] }} - {{ number_format($termek['ar'], 0, ',', ' ') }} Ft</li>
         @endforeach
     </ul>
+
+    <p><strong>Összesen:</strong> {{ number_format($mailData['total'], 0, ',', ' ') }} Ft</p> <!-- 🔥 Az összesített fizetendő összeg -->
 </body>
 </html>
